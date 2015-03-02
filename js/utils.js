7e0af5f45fd52644d76355c6e7df7a226190ea64
starsplatter <jenncd@gmail.com>
2015-03-02 11:50:55 -0500

    remove owlcarousel settings from utils

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,4 @@
-$(document).ready(function() {
-  $("#owl-demo").owlCarousel({
-      autoPlay : false,
-      stopOnHover : true,
-      pagination: false,
-      paginationSpeed : 1000,
-      goToFirstSpeed : 2000,
-      singleItem : true,
-      autoHeight : true,
-      transitionStyle:"fade",
-      navigation:true,
-      navigationText : ["<",">"]
-  });
-});
 
-    
 
 
 
