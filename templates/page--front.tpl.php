cb693a47bc56d39b58906f7482a9961c6455b995
starsplatter <jenncd@gmail.com>
2015-03-05 13:35:06 -0500

    test previous mark up

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -146,35 +146,13 @@ $theme_path = base_path() . 'sites/all/themes/persuasivemaps_bootstrap';
 
 
 
+<div id="owl-demo" class="owl-carousel">
+              <div><img src="../images/map-1.jpg"><div class="legend">Short Title / Date</div></div>
+              <div><a href=""><img src="../images/map-2.jpg"><div class="legend">Map Legend</a></div></div>
+              <div><img src="../images/map-3.jpg"><div class="legend">Map Legend</div></div>
+              <div><img src="../images/map-4.jpg"><div class="legend">Map Legend</div></div>
 
-    <!--<div>
-        <a href=""><img src="<?php print $theme_path; ?>/images/map-1.jpg"></a>
-        <div class="legend"><a href="">Short Title / Date</a></div>
-    </div>
-      
-    <div>
-        <img src="<?php print $theme_path; ?>/images/map-2.jpg">
-        <div class="legend">Map Legend</div>
-    </div>
-      
-    <div>
-        <img src="<?php print $theme_path; ?>/images/map-3.jpg">
-        <div class="legend">Map Legend</div>
-    </div>
-      
-    <div>
-        <img src="<?php print $theme_path; ?>/images/map-4.jpg">
-        <div class="legend">Map Legend</div>
-    </div>
-
-    <div>
-        <img src="<?php print $theme_path; ?>/images/map-4.jpg">
-        <div class="legend">Map Legend</div>
-    </div>-->
-      
-
-
-    <?php print render($page['carousel']); ?>
+        </div>
 
                 
             </div>
