4980a495c2438c3d53b4b3d625f1b2d8cddfaa3d
starsplatter <jenncd@gmail.com>
2015-03-05 12:54:38 -0500

    remove default junk

--- templates/views-view--carousel-maps.tpl.php
+++ templates/views-view--carousel-maps.tpl.php
@@ -28,33 +28,10 @@
  */
 ?>
 <div class="owl-carousel">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
-  <?php endif; ?>
+  
 
   <?php if ($rows): ?>
-    <div class="view-content">
       <?php print $rows; ?>
-    </div>
   <?php elseif ($empty): ?>
     <div class="view-empty">
       <?php print $empty; ?>
